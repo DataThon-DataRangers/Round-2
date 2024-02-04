997866940412cb2cd6652d5d1b3c081ce3bdcbb3
Yash <78804783+yashd-dev@users.noreply.github.com>
2024-02-04 15:00:31 +0530

    Login Page

--- app/Login-Theme/page.js
+++ app/Login-Theme/page.js
@@ -1,9 +1,28 @@
+"use client";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { Input } from "@/components/ui/input";
 
-export default function Fact() {
+import { useEffect, useState } from "react";
+import { auth, provider } from "../firebase/firebase.js";
+import { signInWithPopup, signOut } from "firebase/auth";
+import { useRouter } from "next/navigation";
+
+export default function Login() {
+	const [user, setUser] = useState({});
+	const router = useRouter();
+	const handleClick = async () => {
+		try {
+			const res = await signInWithPopup(auth, provider);
+			console.log(res.user);
+			setUser(res.user);
+			localStorage.setItem("email", JSON.stringify(res.user));
+			router.push("/");
+		} catch (err) {
+			console.error(err);
+		}
+	};
 	return (
 		<section class="bg-white">
 			<div class="lg:grid lg:min-h-screen lg:grid-cols-12">
@@ -36,157 +55,40 @@ export default function Fact() {
 							>
 								<span class="sr-only">Home</span>
 								<svg
-									class="h-8 sm:h-10"
-									viewBox="0 0 28 24"
-									fill="none"
 									xmlns="http://www.w3.org/2000/svg"
+									width="32"
+									height="32"
+									fill="#000000"
+									viewBox="0 0 256 256"
 								>
-									<path
-										d="M0.41 10.3847C1.14777 7.4194 2.85643 4.7861 5.2639 2.90424C7.6714 1.02234 10.6393 0 13.695 0C16.7507 0 19.7186 1.02234 22.1261 2.90424C24.5336 4.7861 26.2422 7.4194 26.98 10.3847H25.78C23.7557 10.3549 21.7729 10.9599 20.11 12.1147C20.014 12.1842 19.9138 12.2477 19.81 12.3047H19.67C19.5662 12.2477 19.466 12.1842 19.37 12.1147C17.6924 10.9866 15.7166 10.3841 13.695 10.3841C11.6734 10.3841 9.6976 10.9866 8.02 12.1147C7.924 12.1842 7.8238 12.2477 7.72 12.3047H7.58C7.4762 12.2477 7.376 12.1842 7.28 12.1147C5.6171 10.9599 3.6343 10.3549 1.61 10.3847H0.41ZM23.62 16.6547C24.236 16.175 24.9995 15.924 25.78 15.9447H27.39V12.7347H25.78C24.4052 12.7181 23.0619 13.146 21.95 13.9547C21.3243 14.416 20.5674 14.6649 19.79 14.6649C19.0126 14.6649 18.2557 14.416 17.63 13.9547C16.4899 13.1611 15.1341 12.7356 13.745 12.7356C12.3559 12.7356 11.0001 13.1611 9.86 13.9547C9.2343 14.416 8.4774 14.6649 7.7 14.6649C6.9226 14.6649 6.1657 14.416 5.54 13.9547C4.4144 13.1356 3.0518 12.7072 1.66 12.7347H0V15.9447H1.61C2.39051 15.924 3.154 16.175 3.77 16.6547C4.908 17.4489 6.2623 17.8747 7.65 17.8747C9.0377 17.8747 10.392 17.4489 11.53 16.6547C12.1468 16.1765 12.9097 15.9257 13.69 15.9447C14.4708 15.9223 15.2348 16.1735 15.85 16.6547C16.9901 17.4484 18.3459 17.8738 19.735 17.8738C21.1241 17.8738 22.4799 17.4484 23.62 16.6547ZM23.62 22.3947C24.236 21.915 24.9995 21.664 25.78 21.6847H27.39V18.4747H25.78C24.4052 18.4581 23.0619 18.886 21.95 19.6947C21.3243 20.156 20.5674 20.4049 19.79 20.4049C19.0126 20.4049 18.2557 20.156 17.63 19.6947C16.4899 18.9011 15.1341 18.4757 13.745 18.4757C12.3559 18.4757 11.0001 18.9011 9.86 19.6947C9.2343 20.156 8.4774 20.4049 7.7 20.4049C6.9226 20.4049 6.1657 20.156 5.54 19.6947C4.4144 18.8757 3.0518 18.4472 1.66 18.4747H0V21.6847H1.61C2.39051 21.664 3.154 21.915 3.77 22.3947C4.908 23.1889 6.2623 23.6147 7.65 23.6147C9.0377 23.6147 10.392 23.1889 11.53 22.3947C12.1468 21.9165 12.9097 21.6657 13.69 21.6847C14.4708 21.6623 15.2348 21.9135 15.85 22.3947C16.9901 23.1884 18.3459 23.6138 19.735 23.6138C21.1241 23.6138 22.4799 23.1884 23.62 22.3947Z"
-										fill="currentColor"
-									/>
+									<path d="M72,136H32a8,8,0,0,1,0-16H67.72L81.34,99.56a8,8,0,0,1,13.32,0l25.34,38,9.34-14A8,8,0,0,1,136,120h24a8,8,0,0,1,0,16H140.28l-13.62,20.44a8,8,0,0,1-13.32,0L88,118.42l-9.34,14A8,8,0,0,1,72,136ZM178,32c-20.65,0-38.73,8.88-50,23.89C116.73,40.88,98.65,32,78,32A62.07,62.07,0,0,0,16,94c0,.75,0,1.5,0,2.25a8,8,0,1,0,16-.5c0-.58,0-1.17,0-1.75A46.06,46.06,0,0,1,78,48c19.45,0,35.78,10.36,42.6,27a8,8,0,0,0,14.8,0c6.82-16.67,23.15-27,42.6-27a46.06,46.06,0,0,1,46,46c0,53.61-77.76,102.15-96,112.8-10.83-6.31-42.63-26-66.68-52.21a8,8,0,1,0-11.8,10.82c31.17,34,72.93,56.68,74.69,57.63a8,8,0,0,0,7.58,0C136.21,220.66,240,164,240,94A62.07,62.07,0,0,0,178,32Z"></path>
 								</svg>
 							</a>
-
-							<h1 class="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-								Welcome to Squid 🦑
-							</h1>
-
-							<p class="mt-4 leading-relaxed text-gray-500">
-								Lorem, ipsum dolor sit amet consectetur
-								adipisicing elit. Eligendi nam dolorum aliquam,
-								quibusdam aperiam voluptatum.
-							</p>
 						</div>
 
-						<form action="#" class="mt-8 grid grid-cols-6 gap-6">
-							<div class="col-span-6 sm:col-span-3">
-								<label
-									for="FirstName"
-									class="block text-sm font-medium text-gray-700"
-								>
-									First Name
-								</label>
-
-								<input
-									type="text"
-									id="FirstName"
-									name="first_name"
-									class="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
+						<form
+							action="#"
+							class="mt-8 flex flex-col justify-center items-start gap-8"
+						>
+							<h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+								Sign In To Continue
+							</h1>
+							<p className="leading-7 max-w-[40ch] ">
+								Dont worry, we wont sell your data, we are using
+								it to store your chat history for convinience
+							</p>
+							<button
+								onClick={handleClick}
+								className="px-4 py-2 border flex gap-2 bg-white border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150"
+							>
+								<img
+									className="w-6 h-6"
+									src="https://www.svgrepo.com/show/475656/google-color.svg"
+									loading="lazy"
+									alt="google logo"
 								/>
-							</div>
-
-							<div class="col-span-6 sm:col-span-3">
-								<label
-									for="LastName"
-									class="block text-sm font-medium text-gray-700"
-								>
-									Last Name
-								</label>
-
-								<input
-									type="text"
-									id="LastName"
-									name="last_name"
-									class="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
-								/>
-							</div>
-
-							<div class="col-span-6">
-								<label
-									for="Email"
-									class="block text-sm font-medium text-gray-700"
-								>
-									{" "}
-									Email{" "}
-								</label>
-
-								<input
-									type="email"
-									id="Email"
-									name="email"
-									class="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
-								/>
-							</div>
-
-							<div class="col-span-6 sm:col-span-3">
-								<label
-									for="Password"
-									class="block text-sm font-medium text-gray-700"
-								>
-									{" "}
-									Password{" "}
-								</label>
-
-								<input
-									type="password"
-									id="Password"
-									name="password"
-									class="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
-								/>
-							</div>
-
-							<div class="col-span-6 sm:col-span-3">
-								<label
-									for="PasswordConfirmation"
-									class="block text-sm font-medium text-gray-700"
-								>
-									Password Confirmation
-								</label>
-
-								<input
-									type="password"
-									id="PasswordConfirmation"
-									name="password_confirmation"
-									class="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
-								/>
-							</div>
-
-							<div class="col-span-6">
-								<label for="MarketingAccept" class="flex gap-4">
-									<input
-										type="checkbox"
-										id="MarketingAccept"
-										name="marketing_accept"
-										class="h-5 w-5 rounded-md border-gray-200 bg-white shadow-sm"
-									/>
-
-									<span class="text-sm text-gray-700">
-										I want to receive emails about events,
-										product updates and company
-										announcements.
-									</span>
-								</label>
-							</div>
-
-							<div class="col-span-6">
-								<p class="text-sm text-gray-500">
-									By creating an account, you agree to our
-									<a href="#" class="text-gray-700 underline">
-										{" "}
-										terms and conditions{" "}
-									</a>
-									and
-									<a href="#" class="text-gray-700 underline">
-										privacy policy
-									</a>
-									.
-								</p>
-							</div>
-
-							<div class="col-span-6 sm:flex sm:items-center sm:gap-4">
-								<button class="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500">
-									Create an account
-								</button>
-
-								<p class="mt-4 text-sm text-gray-500 sm:mt-0">
-									Already have an account?
-									<a href="#" class="text-gray-700 underline">
-										Log in
-									</a>
-									.
-								</p>
-							</div>
+								<span>SignIn with Google</span>
+							</button>
 						</form>
 					</div>
 				</main>
