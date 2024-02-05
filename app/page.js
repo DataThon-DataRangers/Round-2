08910a5211533e9039238c6b042c6f323a0e1ddf
Naitik Lodha <naitik.lodha139@nmims.edu.in>
2024-02-05 10:10:30 +0530

    Changed title

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
 		<main className="bg-background  overflow-hidden text-foreground flex flex-col justify-center items-center h-screen relative ">
 			<div className="bg-card border-2 rounded drop-shadow-lg w-full h-full px-7 md:w-[50vw] md:h-[40vh] flex flex-col justify-center items-center text-center space-y-10 animate-fade-up animate-duration-[1500ms] ">
 				<h1 className="text-4xl font-semibold caret-card-foreground">
-					Welcome to Our Health App
+					Welcome to Our Health App MediBudddy
 				</h1>
 				<Button asChild>
 					<Link href="/Fact">Fact Checker</Link>
